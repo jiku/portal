a7cec7b032594cdc6a47a81703f4b02fc923eb21
B.E. Henriksen <e@jiku.ca>
2017-03-16 12:46:33 +0100

    Changes to shorter, implicit return.

--- src/imports/src/components/app.js
+++ src/imports/src/components/app.js
@@ -4,16 +4,16 @@ import R from 'ramda'
 import ProjectList from './project-list'
 import Parser from './parser'
 
-const App = appState => {
-  return (<div className="container">
+const App = appState => (
+  <div className="container">
     <h1>jiku</h1>
 
     <Parser markdown={appState.markdown} />
 
     <h2>Projects</h2>
     <ProjectList projects={appState.projects} />
-  </div>)
-}
+  </div>
+)
 
 App.render = R.curry((node, props) => ReactDOM.render(<App {...props}/>, node))
 
