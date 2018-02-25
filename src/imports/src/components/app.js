c2b7981e3fb76d3e4a6187420f6d64438d801d0b
B.E. Henriksen <e@jiku.ca>
2018-02-25 11:31:49 +0100

    Adds routes and history.

--- src/imports/src/components/app.js
+++ src/imports/src/components/app.js
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom'
 import R from 'ramda'
 import ProjectList from './project-list'
 import Parser from './parser'
+import { Router, Route, Switch } from 'react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const browserHistory = createBrowserHistory()
+
+const Base = ({ projects }) =>
+  <div>
+    <h2>Projects</h2>
+    <ProjectList projects={ projects } />
+  </div>
 
 const App = appState => (
   <div className="container">
     <h1>jiku</h1>
 
-    <Parser markdown={appState.markdown} />
-
-    <h2>Projects</h2>
-    <ProjectList projects={appState.projects} />
+    <Router history={browserHistory}>
+      <Switch>
+        <Route exact path="/" render={ () => <Base projects={ appState.projects } /> } />
+        <Route exact path="/ama" render={ () => <Parser markdown={ appState.markdown } /> } />
+      </Switch>
+    </Router>
   </div>
 )
 
