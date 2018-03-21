5c3e39d6f43b65879bd2907331042d3892cdda95
B.E. Henriksen <e@jiku.ca>
2018-03-21 23:10:57 +0100

    Replaces react-loading with react-native ActivityIndicator.

--- src/imports/app/components/Projects/index.js
+++ src/imports/app/components/Projects/index.js
@@ -2,7 +2,7 @@ import { ProjectList } from './ProjectList'
 import React from 'react'
 import { graphql, compose } from 'react-apollo'
 import { gql } from 'apollo-boost'
-import Loading from 'react-loading'
+import { Loader } from '../Indicators/Loader'
 
 const Component = ({ data: { loading, projects }}) =>
   !loading ? (
@@ -10,7 +10,7 @@ const Component = ({ data: { loading, projects }}) =>
       <h2>Projects</h2>
       <ProjectList projects={ projects } />
     </div>
-  ) : <Loading type='spin' color='#eee' />
+  ) : <Loader />
 
 const GET_PROJECTS = gql`
   {
