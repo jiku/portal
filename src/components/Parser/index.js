f7ad48cd85646d8c8e96d2048cd1ef7e65f4620f
B.E. Henriksen <e@jiku.ca>
2018-04-01 23:27:15 +0200

    Adds MD styles and headings workaround.

--- src/components/Parser/index.js
+++ src/components/Parser/index.js
@@ -1,31 +1,55 @@
 import { Loader } from '../Indicators/Loader'
-import { MarkdownView as Markdown } from 'react-native-markdown-view'
+import { MarkdownView as Markdown, SMD } from 'react-native-markdown-view'
 import React from 'react'
 import { graphql, compose } from 'react-apollo'
 import { gql } from 'apollo-boost'
 import { View, Text, SectionList, ActivityIndicator } from 'react-native'
 
 const markdownStyles = {
-  heading1: {
+  text: {
+    textAlign: 'center',
+    color: '#777',
+  },
+  heading: {
     fontFamily: 'jikutype-001-Vector',
-    fontSize: 3,
     color: 'rgba(153, 119, 255, 1)'
+  },
+  heading1: {
+    fontSize: 4
+  },
+  heading2: {
+    fontSize: 3
+  },
+  heading3: {
+    fontSize: 2
   },
   link: {
     color: 'rgba(119, 255, 153, 1)',
   },
   mailTo: {
     color: 'rgba(119, 255, 153, 1)',
+  }
+}
+
+const headings = {
+  match: (source, state, lookbehind) => /^(#{1,6})([^\n]+)/.exec(source),
+  // order: () => {},
+  // quality: () => 100,
+  parse: (capture, nestedParse, state) => {
+    return { text: `${capture[1]} ${capture[2]}` };
   },
-  text: {
-    color: '#777',
+  render: (node, output, state, styles) => { // FIX: Styles shouldn't be hardcoded but passed by MarkdownView?
+  const heading = node.text.split('# ')
+  return <Text {...state} style={[styles.text, ...styles.heading, styles[`heading${heading[0].length + 1}`]]}> 
+      { heading[1].toLowerCase() }
+    </Text>
   }
 }
 
 const Component = ({ data: { loading, parser }}) =>
   !loading?
     <SectionList
-      renderItem={({item}) => <View><Markdown styles={markdownStyles}>{ item }</Markdown></View>}
+      renderItem={({item}) => <View style={{ padding: 30 }}><Markdown rules={{ headings }} styles={markdownStyles}>{ item }</Markdown></View>}
       sections={[
         { keyExtractor: (item, index) => `${item}:${index}`, data: [parser.markdown], title: 'AMA'}
       ]}
